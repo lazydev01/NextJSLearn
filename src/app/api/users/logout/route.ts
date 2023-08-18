691bb2e4d2cde3c4a7c90fdff29c63758f602ac4
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-18 22:41:20 +0530

    Refactored Login Page UI and added route for Logout

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+
+export async function GET(){
+    try {
+        
+    } catch (error : any) {
+        return NextResponse.json({
+            error : error.message,
+        }, {status : 500});
+    }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,46 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: ""
   });
 
-  const onLogin = async () => {};
+  useEffect(()=> {
+    if(user.email.length>0 && user.password.length>0){
+      setButtonDisabled(false);
+    }
+    else{
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try{
+      setLoading(true);
+      const response = await axios.post('/api/users/login', user);
+      console.log("Login successful", response.data);
+      toast.success("Login Success");
+      router.push("/profile");
+    }
+    catch(err : any){
+      console.log("Login failed", err.message);
+      toast.error(err.message);
+    }
+    finally{
+      setLoading(false);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Login</h1>
@@ -19,8 +48,8 @@ export default function LoginPage() {
       <label htmlFor="username">Username</label>
       <input type="text" id="email" value={user.email} onChange={(e)=> setUser({...user, email : e.target.value})} placeholder="Email" />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" value={user.email} onChange={(e)=> setUser({...user, password : e.target.value})} placeholder="Password" />
-      <button onClick={onLogin}>LogIn</button>
+      <input type="password" id="password" value={user.password} onChange={(e)=> setUser({...user, password : e.target.value})} placeholder="Password" />
+      <button onClick={onLogin} disabled={buttonDisabled}>LogIn</button>
       <Link href="/signup">Visit Sign Up Page</Link>
     </div>
   );
