3a46408e9da12ca0cdf24475e97cffc45062b3e4
lazydev01 <arpeetbhavsar01@gmail.com>
2023-09-07 21:26:33 +0530

    [Completed Verify Email UI and tested it via MailTrap]

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -11,11 +11,48 @@ export default function VerifyEmailPage(){
 
     const verifyUserEmail = async() => {
         try {
-            
+            await axios.post("/api/users/verifyemail", {token});
+            setVerified(true);
         } catch (error : any) {
             setError(true);
             console.log(error.response.data);
             
         }
     }
+
+    useEffect(()=> {
+        const urlToken = window.location.search.split("=")[1];
+        setToken(urlToken || "");
+    }, []);
+
+    useEffect(()=> {
+        if(token.length>0){
+            verifyUserEmail();
+        }
+    }, [token]);
+
+    return (
+        <div>
+            <h1>Verify Your Email</h1>
+            <h2>{token ? `${token}` : "No Token"}</h2>
+            {verified && (
+                <div>
+                    <h2>
+                        Email Verified
+                    </h2>
+                    <Link href="/login">
+                        Login
+                    </Link>
+                </div>
+            )}
+            {error && (
+                <div>
+                    <h2>
+                        Error
+                    </h2>
+                    <p>{error}</p>
+                </div>
+            )}
+        </div>
+    )
 }
